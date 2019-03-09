7adf55ab68978095dff62ef3564ddf477bf0411b
Iaroslav Koren <koryar@ukr.net>
2019-03-09 22:03:41 +0200

    [Task 1] Core concepts lecture. Use at least 2 methods of creating react components.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Title from './components/Title';
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                netflixroulette
+                <Title />
+            </React.Fragment>
+        );
+    }
+}
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Title() {
+    return <h1>Find your movie</h1>;
+}
+export default Title;
