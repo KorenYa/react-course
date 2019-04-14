11bc903986872961fd2110502f61d8be5f5d1d94
Iaroslav Koren <koryar@ukr.net>
2019-04-14 21:04:01 +0300

    [Task 5] Flux + Redux. Store data offline with redux-persist.

--- src/Root.js
+++ src/Root.js
@@ -2,11 +2,16 @@ import React from 'react';
 import App from './components/App';
 import store from './store';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function Root() {
+    const persistor = persistStore(store);
     return (
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     );
 }
