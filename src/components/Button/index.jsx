6f71ed88e7bb5cd663d41d40cf91eaf090445651
Iaroslav Koren <koryar@ukr.net>
2019-05-17 00:52:58 +0300

    [Task 8] Useful libraries. Use flow instead of Prop Types for Button component.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,17 @@
+// @flow
 import React from 'react';
-import PropTypes from 'prop-types';
 import './button.scss';
 
-const Button = (props) => {
-  const {
-    onClick, searchBy, styleName, type, text,
-  } = props;
+type ButtonProps = {
+  type: string,
+  text: string,
+  searchBy: string,
+  styleName: string,
+  onClick: func,
+};
+
+const Button = (props: ButtonProps) => {
+  const { onClick, searchBy, styleName, type, text } = props;
 
   return (
     <button
@@ -18,11 +24,5 @@ const Button = (props) => {
     </button>
   );
 };
-Button.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  searchBy: PropTypes.string,
-  styleName: PropTypes.string,
-  onClick: PropTypes.func,
-};
+
 export default Button;
