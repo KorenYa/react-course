13cc783dbd9216f19a4a777fe17481375d01dfed
iaroslav_koren <iaroslav_koren@epam.com>
2019-05-17 16:36:40 +0300

    [Task 8] Useful libraries. Convert NotFound component to styled component.

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import './notfound.scss';
+import styled from 'styled-components';
+
+const StyledNotFound = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #3c3c3c;
+  font-size: 60px;
+  font-weight: 600;
+  height: 100vh;
+`;
 
 const NotFound = () => (
-  <div className="not-found-page">
+  <StyledNotFound>
     <span>404 - Not found</span>
-  </div>
+  </StyledNotFound>
 );
 
 export default NotFound;
